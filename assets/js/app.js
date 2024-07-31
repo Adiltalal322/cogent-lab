d77954e504d59b81f1a86d314bebf15e7378b8da
Adil Talal <adil@Muhammad.Adil.Talal>
2024-08-01 00:48:36 +0500

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,20 @@ document.getElementById('menu-btn').addEventListener('click', function() {
 document.getElementById('close-btn').addEventListener('click', function() {
     document.getElementById('sidenav').classList.remove('open');
 });
+
+     w = $(window).width()
+    xl = 1440
+    md = 992
+    ws = (w / 1440) * 10
+    if (w >= md) {
+        $("html").css("font-size", ws + "px")
+    }
+    $(window).on("resize", function () {
+        w = $(window).width()
+        xl = 1440
+        md = 992
+        ws = (w / 1440) * 10
+        if (w >= md) {
+            $("html").css("font-size", ws + "px")
+        }
+    })
